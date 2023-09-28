edf862377642bbf495ce8bd85262609f072a3b24
Frederic Esser <frederic.esser@googlemail.com>
2023-09-28 10:02:52 +0200

    Initial commit

---
+++ src/main/java/net/fribbtastic/learningSpringBoot/LearningSpringBootApplication.java
@@ -0,0 +1,13 @@
+package net.fribbtastic.learningSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fribbtastic/learningSpringBoot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fribbtastic.learningSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
