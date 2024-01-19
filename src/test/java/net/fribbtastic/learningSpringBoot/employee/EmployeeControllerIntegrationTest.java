9b582c1792c4726c760823505a17033d3c8e9610
Frederic Esser <frederic.esser@googlemail.com>
2024-01-19 09:55:50 +0100

    added integration tests for the Employee Controller

--- src/test/java/net/fribbtastic/learningSpringBoot/employee/EmployeeControllerIntegrationTest.java
+++ src/test/java/net/fribbtastic/learningSpringBoot/employee/EmployeeControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package net.fribbtastic.learningSpringBoot.employee;
 
 import net.fribbtastic.learningSpringBoot.LearningSpringBootApplication;
+import net.fribbtastic.learningSpringBoot.exceptions.EntityNotFoundException;
 import net.fribbtastic.learningSpringBoot.responses.ApiResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -28,6 +29,9 @@ public class EmployeeControllerIntegrationTest {
     @Autowired
     private TestRestTemplate template;
 
+    /**
+     * Integration test to get all Employee from the Employee endpoint
+     */
     @DisplayName("Test [Integration]: get All Employees")
     @Sql({"classpath:employee/truncate.sql"}) // run a database script when executing this test
     @Sql({"classpath:employee/insert.sql"})
@@ -52,6 +56,9 @@ public class EmployeeControllerIntegrationTest {
         Assertions.assertThat(responseEntity.getBody().getData().get(2).getLastName()).isEqualTo("Test LastName 03");       // assert that the lastName is as Expected
     }
 
+    /**
+     * Integration Test to get an Empty list of Employees
+     */
     @DisplayName("Test [Integration]: get All Employees (empty list)")
     @Sql({"classpath:employee/truncate.sql"})
     @Test
@@ -64,5 +71,54 @@ public class EmployeeControllerIntegrationTest {
         Assertions.assertThat(responseEntity.getBody().getStatusCode()).isEqualTo(200);     // assert that the JSON element statusCode is 200
         Assertions.assertThat(responseEntity.getBody().getData()).isNotNull();                      // assert that the JSON element data is not null
         Assertions.assertThat(responseEntity.getBody().getData().size()).isEqualTo(0);      // assert that the JSON element data has 0 elements
+    }
+
+    /**
+     * Integration Test to get one specific Employee from the Employee Endpoint
+     */
+    @DisplayName("Test [Integration]: get one Employee")
+    @Sql({"classpath:employee/truncate.sql"})
+    @Sql({"classpath:employee/insert.sql"})
+    @Test
+    public void testGetOneEmployee() {
+
+        long id = 1L;
+
+        // Make the GET request to the API for the Employee with a specific ID
+        ResponseEntity<ApiResponse<Employee>> response = this.template.exchange("/employee/" + id, HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+        });
+
+        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(200);                                 // assert that the response status code is 200
+        Assertions.assertThat(response.getBody()).isNotNull();                                                          // assert that the response body is not null
+        Assertions.assertThat(response.getBody().getStatusCode()).isEqualTo(200);                               // assert that the JSON element statusCode is 200
+        Assertions.assertThat(response.getBody().getData()).isNotNull();                                                // assert that the JSON element data is not null
+        Assertions.assertThat(response.getBody().getData().getFirstName()).isEqualTo("Test FirstName 01");      // assert that the firstName is set as expected
+        Assertions.assertThat(response.getBody().getData().getLastName()).isEqualTo("Test LastName 01");        // assert that the lastName is set as expected
+
+    }
+
+    /**
+     * Integration Test to get a missing Employee
+     */
+    @DisplayName("Test [Integration]: get one missing Employee")
+    @Sql({"classpath:employee/truncate.sql"})
+    @Test
+    public void testGetOneEmployee_Empty() {
+
+        long id = 1L;
+
+        // Make the GET request to the API for the Employee with a specific ID
+        ResponseEntity<ApiResponse<Employee>> response = this.template.exchange("/employee/" + id, HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+        });
+
+        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(404);                                 // assert that the response status code is 404
+        Assertions.assertThat(response.getBody()).isNotNull();                                                          // assert that the response body is not null
+        Assertions.assertThat(response.getBody().getStatusCode()).isEqualTo(404);                               // assert that the JSON element statusCode is 404
+        Assertions.assertThat(response.getBody().getErrorDetails()).isNotNull();                                        // assert that there is an error JSON element
+        Assertions.assertThat(response.getBody().getErrorDetails().getType()).isEqualTo(EntityNotFoundException.class.getSimpleName()); // assert that the type JSON element is set as expected
+        Assertions.assertThat(response.getBody().getErrorDetails().getMessage()).isEqualTo("Entity not found"); // assert that the message JSON element is set ass expected
+        Assertions.assertThat(response.getBody().getErrorDetails().getDetails()).isEqualTo("Entity with the ID '" + id + "' could not be found"); // assert that the details JSON element is set as expected
+        Assertions.assertThat(response.getBody().getErrorDetails().getTimestamp()).isNotEmpty();                        // assert that the timestamp JSON element is not empty
+
     }
 }
